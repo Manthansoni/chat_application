eb42d41561f202749e8c02f26498b7a0ac2ebc66
Manthan <manthan04soni@gmail.com>
2024-09-06 09:20:15 +0530

    third commit

--- chat_application/settings.py
+++ chat_application/settings.py
@@ -138,6 +138,7 @@ ASGI_APPLICATION = 'chat_application.asgi.application'
 
 REDIS_URL = os.environ['REDIS_URL']
 # REDIS_URL = "redis://127.0.0.1:6379/"
+
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
